461a93b828015df157c706ba8ff4adcdac0ef3a8
Tim Kelty <tim@craftcms.com>
2024-02-12 13:25:15 -0500

    Allow non-URL assets to be transformed

--- src/ImageTransformer.php
+++ src/ImageTransformer.php
@@ -31,10 +31,6 @@ class ImageTransformer extends Component implements ImageTransformerInterface
         $assetUrl = Html::encodeSpaces(Assets::generateUrl($volume, $this->asset));
         $mimeType = $asset->getMimeType();
 
-        if (!$fs->hasUrls) {
-            throw new NotSupportedException('The asset’s volume’s transform filesystem doesn’t have URLs.');
-        }
-
         if ($mimeType === 'image/gif' && !Craft::$app->getConfig()->getGeneral()->transformGifs) {
             throw new NotSupportedException('GIF files shouldn’t be transformed.');
         }
