c736774b50adfe104d12b0277b795a8049cb30a2
Tim Kelty <tim@craftcms.com>
2024-06-28 16:25:09 -0400

    Default fit mode should always be crop

--- src/ImageTransformer.php
+++ src/ImageTransformer.php
@@ -113,7 +113,7 @@ class ImageTransformer extends Component implements ImageTransformerInterface
             'fit' => $imageTransform->upscale ? 'contain' : 'scale-down',
             'stretch' => 'cover',
             'letterbox' => 'pad',
-            default => $imageTransform->upscale ? 'cover' : 'crop',
+            default => 'crop',
         };
     }
 
