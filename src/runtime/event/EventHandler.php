ac8d572de43137443ce93ac2e31814cc3ab46de6
Tim Kelty <tim@craftcms.com>
2025-07-03 17:46:48 -0400

    Set in module

--- src/runtime/event/EventHandler.php
+++ src/runtime/event/EventHandler.php
@@ -10,11 +10,6 @@ class EventHandler implements Handler
 {
     private FpmHandler $fpmHandler;
 
-    # include buffer, so PHP dies before Lambda
-    private const MAX_SECONDS = 900 - 3;
-
-    private const MAX_HTTP_SECONDS = 60;
-
     public function __construct(FpmHandler $fpmHandler)
     {
         $this->fpmHandler = $fpmHandler;
@@ -22,8 +17,6 @@ class EventHandler implements Handler
 
     public function handle(mixed $event, Context $context)
     {
-        ini_set('max_execution_time', self::MAX_SECONDS);
-
         // is this a sqs event?
         if (isset($event['Records'])) {
             return (new SqsHandler())->handle($event, $context);
@@ -33,8 +26,6 @@ class EventHandler implements Handler
         if (isset($event['command'])) {
             return (new CliHandler())->handle($event, $context);
         }
-
-        ini_set('max_execution_time', self::MAX_HTTP_SECONDS);
 
         return $this->fpmHandler->handle($event, $context);
     }
